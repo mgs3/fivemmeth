15813d69c64353147a4777473bd687fd6dd2c1d0
MGS-3 <logan@lavigne.xyz>
2025-03-23 19:00:58 +0100

    ajout vol

--- vol/fxmanifest.lua
+++ vol/fxmanifest.lua
@@ -1,6 +1,6 @@
 version '1.0.0'
 author 'mgs3'
-description 'Demare la transformation de la meth'
+description 'Vol a l\'étalage (Ne pas prendre en compte ce code pour le moment)'
 
 client_script 'client.lua'
 server_script 'server.lua'
